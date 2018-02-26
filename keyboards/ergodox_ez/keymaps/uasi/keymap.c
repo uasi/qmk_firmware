4e70f33af5471e0375068faeb08ef1a03a70cb43
Tomoki Aonuma <uasi@uasi.jp>
2018-02-26 16:01:42 +0900

    [uasi] Update henkan/muhenkan keymaps

--- keyboards/ergodox_ez/keymaps/uasi/keymap.c
+++ keyboards/ergodox_ez/keymaps/uasi/keymap.c
@@ -13,11 +13,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = KEYMAP(
         // left hand
         KC_ESC,      KC_1,         KC_2,    KC_3,   KC_4,    KC_5,   TO(LFTY),
-        KC_TAB,      KC_Q,         KC_W,    KC_E,   KC_R,    KC_T,   JP_MHEN,
+        KC_TAB,      KC_Q,         KC_W,    KC_E,   KC_R,    KC_T,   KC_TRNS,
         KC_LCTL,     KC_A,         KC_S,    KC_D,   KC_F,    KC_G,
         KC_LSFT,     KC_Z,         KC_X,    KC_C,   KC_V,    KC_B,   JP_LBRC,
-        KC_LGUI,     KC_TRNS,      KC_LALT,         KC_LGUI, KC_LCTRL,
-                                                    KC_MHEN, KC_TRNS,
+        KC_LGUI,     KC_TRNS,      KC_LALT,         KC_LGUI, KC_MHEN,
+                                                    KC_TRNS, KC_TRNS,
                                                              KC_TRNS,
                                             KC_SPC, KC_BSPC, LALT(KC_SPC),
         // right hand
@@ -25,8 +25,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         JP_HENK,     KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,             JP_AT,
                      KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN,          JP_COLN,
         JP_RBRC,     KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          JP_BSLS,
-                             KC_LALT, KC_RGUI, KC_TRNS, KC_RCTL,          JP_YEN,
-        KC_TRNS,        KC_HENK,
+                             KC_HENK, KC_RGUI, KC_TRNS, KC_RCTL,          JP_YEN,
+        KC_TRNS,        KC_TRNS,
         KC_TRNS,
         KC_TRNS, KC_BSPC, LT(SYMB, KC_ENT)
     ),
