8026f978f33c797367e565e2640c4e36ce74c33c
Tomoki Aonuma <uasi@uasi.jp>
2018-02-28 15:44:48 +0900

    [uasi] Assign another L-ALT

--- keyboards/ergodox_ez/keymaps/uasi/keymap.c
+++ keyboards/ergodox_ez/keymaps/uasi/keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,      KC_Q,         KC_W,    KC_E,   KC_R,    KC_T,   KC_TRNS,
         KC_LCTL,     KC_A,         KC_S,    KC_D,   KC_F,    KC_G,
         KC_LSFT,     KC_Z,         KC_X,    KC_C,   KC_V,    KC_B,   JP_LBRC,
-        KC_LGUI,     KC_TRNS,      KC_LALT,         KC_LGUI, KC_MHEN,
+        KC_LGUI,     KC_LALT,      KC_LALT,         KC_LGUI, KC_MHEN,
                                                     KC_TRNS, KC_TRNS,
                                                              KC_TRNS,
                                             KC_SPC, KC_BSPC, LALT(KC_SPC),
